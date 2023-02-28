be0bc1c3fb8771060276311b10b42f87b95795a1
sonali <sonalibhavsar977@gmail.com>
2023-02-28 15:02:46 +0530

    Created seed files for sample data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times { |blog|
+  Blog.create!(
+    title: "My Blog Post #{blog}",
+    body: "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32."
+    )
+}
+
+puts "10 blog post created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "Rails #{Skill}",
+    percent_utilized: 76)
+end
+
+puts "5 skills created"
+
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "portfolio titles #{portfolio_item}",
+    subtitle: "my great service",
+    body: "ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de Finibus Bonorum et Malorum' (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, 'Lorem ipsum dolor sit amet..', comes from a line in section 1.10.32.",
+    main_image:"http://placehold.it/600*200",
+    thumb_image:"http://placehold.it/300*200"
+    )
+end
+
+puts "9 portfolio created"
